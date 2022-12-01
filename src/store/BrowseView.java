753a1d9db26006819a2fff90970a60df8ec39828
Pedromex2007 <34046361+Pedromex2007@users.noreply.github.com>
2022-12-01 16:54:10 -0500

    Update BrowseView.java

--- src/store/BrowseView.java
+++ src/store/BrowseView.java
@@ -11,10 +11,11 @@ import java.awt.FlowLayout;
 import javax.swing.JLayeredPane;
 import java.awt.Color;
 import java.awt.CardLayout;
+import javax.swing.JLabel;
 
 public class BrowseView extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel masterPanel;
 
 	/**
 	 * Launch the application.
@@ -38,23 +39,52 @@ public class BrowseView extends JFrame {
 	public BrowseView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 761, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		masterPanel = new JPanel();
+		masterPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+		setContentPane(masterPanel);
+		masterPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 		
-		JPanel panel = new JPanel();
-		contentPane.add(panel);
-		panel.setLayout(new CardLayout(50, 50));
+		JPanel subPanel = new JPanel();
+		masterPanel.add(subPanel);
+		subPanel.setLayout(new GridLayout(0, 2, 0, 0));
 		
-		JButton btnNewButton = new JButton("New button");
-		panel.add(btnNewButton, "name_22670970460400");
+		JLabel lblNewLabel = new JLabel("ITEM_NAME:");
+		subPanel.add(lblNewLabel);
+		
+		JButton btnView = new JButton("View");
+		subPanel.add(btnView);
+		
+		JLabel lblItemprice = new JLabel("ITEM_PRICE:");
+		subPanel.add(lblItemprice);
+		
+		JButton btnAddToCart = new JButton("Add to Cart");
+		subPanel.add(btnAddToCart);
+		GenerateStoreButtons();
 	}
 	
 	private void GenerateStoreButtons() {
 		//TODO: Buttons with their JLabels will be generated here. Each button will contain information about a specific item retrieved from the database.
-		// Clicking on any of the buttons will instantiate the DetailView. Information from the item will be transferred to this view.
+		// Clicking on the "view" button will instantiate the DetailView. Information from the item will be transferred to this view.
+		
+		//The for loop is an example on how to instantiate each container. We would iterate over each item our database has instead.
+		for(int i = 0; i < 10; i++) {
+			JPanel subPanel = new JPanel();
+			masterPanel.add(subPanel);
+			subPanel.setLayout(new GridLayout(0, 2, 0, 0));
+			
+			JLabel lblNewLabel = new JLabel("ITEM_NAME:");
+			subPanel.add(lblNewLabel);
+			
+			JButton btnView = new JButton("View");
+			subPanel.add(btnView);
+			
+			JLabel lblItemprice = new JLabel("ITEM_PRICE:");
+			subPanel.add(lblItemprice);
+			
+			JButton btnAddToCart = new JButton("Add to Cart");
+			subPanel.add(btnAddToCart);
+		}
 	}
 
 }
